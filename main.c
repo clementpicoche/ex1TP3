38c7efb0794edd62e94d979bf32a01296718bbe3
clementpicoche <clement.picoche@edu.ece.fr>
2023-10-12 11:28:25 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+    int a = 0;
+    int b = 0;
+    printf("saisir deux entier:\n");
+    scanf("%d%d",&a,&b);
+
+    if(a>b){
+        printf("l,entier le plus grand est:%d",a);}
+    else{
+        printf("l,entier le plus grand est:%d",b);
+    }
+
+
+   return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
